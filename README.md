ce164cdabd23dae36a55180eeaabef4e1d9dad0a
Clayton Daley <clayton.daley@gmail.com>
2013-10-16 14:42:27 -0400

    Enhances the delete action example in API-Action:  - Forces authentication  - Adds internal check to support both full sURLs and keywords

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ yourls-api-delete
 =================
 
 Plugin for YOURLS adding a "delete" action to the API
+
+# What for
+
+This plug-in adds the "delete" action to the YOURLS API.
+`http://my.url/yourls-api.php?username=x&password=xx&action=delete&keyword=abc123&format=json` 
+
+# How to
+
+* In `/user/plugins`, create a new folder named `yourls-api-delete`
+* Drop these files in that directory
+* Go to the Plugins administration page and activate the plugin 
+* Have fun
---
+++ plugin.php
@@ -0,0 +1,64 @@
+<?php
+/*
+Plugin Name: API Action - Delete
+Plugin URI: http://www.claytondaley.com/
+Description: Adds a "delete" action to the API. This action requires authentication even if the site is public. This action accepts either a shorturl or keyword passed using the "shorturl" parameter.
+Version: 1.0
+Author: Clayton Daley (derived from API-Action by Ozh)
+Author URI: http://www.claytondaley.com/
+*/
+
+// Define custom action "delete"
+yourls_add_filter( 'api_action_delete', 'my_delete_function' );
+
+// Actually delete
+function my_delete_function() {
+	// We don't want unauthenticated users deleting links
+	// If YOURLS is in public mode, force authentication anyway
+	if (!yourls_is_private()) {
+		yourls_do_action( 'require_auth' );
+		require_once( YOURLS_INC.'/auth.php' );
+	}
+
+	// Need 'shorturl' parameter
+	if( !isset( $_REQUEST['shorturl'] ) ) {
+		return array(
+			'statusCode' => 400,
+			'simple'     => "Need a 'shorturl' parameter",
+			'message'    => 'error: missing param',
+		);	
+	}
+	
+	$shorturl = $_REQUEST['shorturl'];
+
+	// Check if valid shorturl
+	if( !yourls_is_shorturl( $shorturl ) ) {
+		return array(
+			'statusCode' => 404,
+			'simple '    => 'Error: short URL not found',
+			'message'    => 'error: not found',
+		);	
+	}
+	
+	// Is $shorturl a URL (http://sho.rt/abc) or a keyword (abc) ?
+	if( yourls_get_protocol( $shorturl ) ) {
+		$keyword = yourls_get_relative_url( $shorturl );
+	} else {
+		$keyword = $shorturl;
+	}
+	
+	// Delete shorturl
+	if( yourls_delete_link_by_keyword( $keyword ) ) {
+		return array(
+			'statusCode' => 200,
+			'simple'     => "Shorturl $shorturl deleted",
+			'message'    => 'success: deleted',
+		);	
+	} else {
+		return array(
+			'statusCode' => 500,
+			'simple'     => 'Error: could not delete shorturl, not sure why :-/',
+			'message'    => 'error: unknown error',
+		);	
+	}
+}
